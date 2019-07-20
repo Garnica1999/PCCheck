26cadb1daac3779f1ce38d8378271677ce62e86d
Garnica1999 <wcarlosfg.1234567890@hotmail.com>
2019-07-19 21:31:40 -0500

    Update Support for multiples OS
    Support for Windows, Solaris, Unix, Linux and Mac

---
+++ PC-CheckServer/src/main/java/com/he/pc/controller/logic/Utilities.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.he.pc.controller.logic;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Clase que almacena metodos utilitarios para la consola y el programa en general
+ * @author Garnica1999
+ */
+public class Utilities {
+    public static final String OS = System.getProperty("os.name").toLowerCase();
+    /**
+     * Limpia la consola de texto anterior
+     */
+    public static void clear() {
+        try {
+            if (isWindows()) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else if (isUnix() || isMac() || isSolaris()) {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(Utilities.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static boolean isWindows() {
+        return (OS.indexOf("win") >= 0);
+    }
+
+    public static boolean isMac() {
+        return (OS.indexOf("mac") >= 0);
+    }
+
+    public static boolean isUnix() {
+        return (OS.indexOf("nix") >= 0) || (OS.indexOf("nux") >= 0) || (OS.indexOf("aix") > 0);
+    }
+
+    public static boolean isSolaris() {
+
+        return (OS.indexOf("sunos") >= 0);
+
+    }
+
+}
