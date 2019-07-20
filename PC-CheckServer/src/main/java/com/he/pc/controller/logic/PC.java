58fa059491b65d8a1a8495d1dd8cb0ee7be2df22
Garnica1999 <wcarlosfg.1234567890@hotmail.com>
2019-07-19 21:31:10 -0500

    Update PC Class
    First, the class has been moved to the controller package.
    In addition, the sending of the GPU class with its serialized data per TCP / IP Sockets volume has been added.
    The way of obtaining the hardware data by other classes has also been modified. Now, these classes are in the same driver package, and the fetch functions are no longer "static" static

--- PC-CheckServer/src/main/java/com/he/pc/controller/logic/PC.java
+++ PC-CheckServer/src/main/java/com/he/pc/controller/logic/PC.java
@@ -6,26 +6,20 @@
 package com.he.pc.controller.logic;
 
 import com.he.pc.controller.logic.hardware.oshi.SystemInfo;
-import com.he.pc.controller.logic.hardware.oshi.hardware.CentralProcessor;
-import com.he.pc.controller.logic.hardware.oshi.hardware.GlobalMemory;
 import com.he.pc.controller.logic.hardware.oshi.hardware.HardwareAbstractionLayer;
 import com.he.pc.model.CPU;
-import com.he.pc.model.CPUtilities;
-import com.he.pc.model.Core;
+import com.he.pc.model.GPU;
 import com.he.pc.model.RAM;
-import com.he.pc.model.RAMUtilities;
 import com.he.pc.view.Main;
+import com.profesorfalken.jsensors.JSensors;
+import com.profesorfalken.jsensors.model.components.Components;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.hyperic.sigar.CpuPerc;
-import org.hyperic.sigar.Sigar;
-import org.hyperic.sigar.SigarException;
-
+import org.slf4j.LoggerFactory;
 /**
  * <p>Clase la cual se encarga de recopilar los datos de hardware del PC y enviarlo
  * mediante sockets</p>
@@ -33,20 +27,9 @@ import org.hyperic.sigar.SigarException;
  * @author Garnica1999
  */
 public class PC {
-    /**
-     * Campo para gardar los datos de la clase Sigar
-     */
-    private final Sigar sigar;
+    
     /**
      * Campo que guarda los datos del CPU
-     */
-    private CPU cpuMain;
-    /**
-     * Campo que guarda los datos de la memoria RAM
-     */
-    private RAM ram;
-    /**
-     * Campo para inicializar el acceso al hardware
      */
     private SystemInfo si;
     /**
@@ -54,23 +37,34 @@ public class PC {
      */
     private HardwareAbstractionLayer hal;
     /**
-     * Campo para obtener la informacion del procesador
+     * Cargar sensores de la CPU y GPU
      */
-    private CentralProcessor processor;
+    private Components components;
     /**
-     * Campo para guardar la informacion de la memoria RAM
+     * 
      */
-    private GlobalMemory memory;
+    private RamController ramController;
+    
     /**
-     * Campo utilizado para obtener temperaturas, carga, e informacion del CPU
+     * 
      */
-    private CPUtilities cpuUtilities;
-
+    private CpuController cpuController;
+    
+    /**
+     * 
+     */
+    private GpuController gpuController;
+    
+    /**
+     * Logger
+     */
+    private org.slf4j.Logger log;
+    
     /**
      * Constructor por defecto
      */
     public PC() {
-        this.sigar = new Sigar();
+        this.loadLogger();
         this.prepareHardware();
         this.sendData();
     }
@@ -79,11 +73,13 @@ public class PC {
      * <p>Metodo para instanciar los campos de esta clase</p>
      */
     private void prepareHardware(){
+        log.info("Preparando dispositivos...");
         this.si = new SystemInfo();
         this.hal = si.getHardware();
-        this.processor = hal.getProcessor();
-        this.memory = hal.getMemory();
-        this.cpuUtilities = new CPUtilities();
+        this.ramController = new RamController(hal);
+        this.cpuController = new CpuController(hal);
+        this.gpuController = new GpuController();
+        log.info("Dispositivos cargados");
     }
     
     /**
@@ -100,106 +96,82 @@ public class PC {
             //Puerto 5000
             int puerto = 5000;
             serverSocket = new ServerSocket(puerto);
-            System.out.println("Establishing connection with the PC-CHECK client");
+            log.info("Establishing connection with the PC-CHECK client");
             socket = serverSocket.accept();
-            System.out.println("Successfully established connection");
-            System.out.println("Starting... It may take several seconds");
+            log.info("Successfully established connection");
+            log.info("Starting... It may take several seconds");
             oos = new ObjectOutputStream(socket.getOutputStream());
             while (true) {
+                //actualizar informacion del hardware
+                this.updateHardware();
                 //Obtencion del hardware
-                this.cpuMain = this.loadCPU();
-                this.ram = this.loadRAM();
+                CPU cpu = this.cpuController.loadCPU();
+                RAM ram = this.ramController.loadRAM();
+                
+                this.gpuController.loadGpu(components);
+                GPU[] gpus = this.gpuController.getAllGpus();
+                
                 //Envio de datos al pc cliente
-                oos.writeObject(this.cpuMain);
-                oos.writeObject(this.ram);
+                log.info("Enviando objeto CPU...");
+                oos.writeObject(cpu);
+                log.info("Enviando objeto RAM...");
+                oos.writeObject(ram);
+                log.info("Enviando objetos GPUs...");
+                oos.writeObject(gpus);
             }
         } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(PC.class.getName()).log(Level.SEVERE, null, ex);
+            log.error(ex.toString());
+            System.err.println("ERROR GRAVE: " + ex.toString());
         } finally {
             try {
                 if(oos != null){
+                    log.info("Cerrando Flujo de objetos");
                     oos.close();
                 }
                 if(socket != null){
+                    log.info("Cerrando conexion");
                     socket.close();
                 }
                 if(serverSocket != null){
+                    log.info("Cerrando servidor");
                     serverSocket.close();
                 }
             } catch (IOException ex) {
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(PC.class.getName()).log(Level.SEVERE, null, ex);
+                log.error(ex.toString());
+                System.err.println("ERROR GRAVE: " + ex.toString());
             }
         }
     }
-    /**
-     * <p>Carga la informacion completa del CPU, incluyendo modelos, frencuencias, 
-     * fabricantes, uso del CPU, uso de los cores, cantidad de nucleos logicos y 
-     * fisicos.</p>
-     * @return Objeto de la clase CPU con informacion del procesador
-     */
-    private CPU loadCPU() {
-        try {
-            //actualizar informacion del hardware
-            this.updateHardware();
-            
-            /*Declarar, instanciar e inicializar una lista para guardar la 
-            informacion de los nucleos*/
-            ArrayList<Core> cores = new ArrayList<>();
-            /*
-            Obtener informacion del CPU
-            */
-            String model = CPUtilities.getCpuName(processor);
-            String vendor = CPUtilities.getCpuVendor(processor);
-            double cpuUse = CPUtilities.getCpuUseNorm(processor);
-            long frecuency = CPUtilities.getCpuFrecuency(processor);
-            int physicalCores = CPUtilities.getCpuCoresCount(processor);
-            int logicalCores = CPUtilities.getCpuLogicalCoresCount(processor);
-            CpuPerc[] porcent = this.sigar.getCpuPercList();
-            //Instanciar e inicializar objeto c del CPU
-            CPU c = new CPU(model, vendor, physicalCores, logicalCores, frecuency, cpuUse, this.cpuUtilities.getTempCpuCore(porcent.length));
-           
-            //Guardar la informacion de todos los nucleos del procesador
-            
-            for(int core = 0; core < porcent.length; core++){
-                //CpuInfo cpu = infos[core];
-                
-                cores.add(new Core(core, porcent[core].getCombined(), this.cpuUtilities.getTempCpuCore(core)));
-                
-            }
-            //Agregar la lista de nucleos al objeto de la clase CPU
-            c.setCores(cores);
-            return c;
-        } catch (SigarException ex) {
-            Logger.getLogger(PC.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
-    }
+    
     /**
      * Actualiza el hardware de la maquina a los ultimos datos
      */
     private void updateHardware(){
-        this.processor.updateAttributes();
-        this.memory.updateAttributes();
-        this.cpuUtilities.updateTemperatures();
+        log.info("Actualizando dispositivos...");
+        this.updateSensors();
+        this.cpuController.updateData();
+        this.cpuController.updateTemperatures(this.components);
+        this.ramController.updateData();
+        log.info("Dispositivos de hardware actualizados");
     }
-    /**
-     * Obtiene la informacion de uso de la memoria RAM del equipo
-     * @return Un objeto de la clase RAM, guardando la informacion de uso de la
-     * memoria ram
-     */
-    private RAM loadRAM(){
-        /*
-        Obtencion de los datos de uso de la memoria RAM (ram usada, disponible, 
-        total, porcentado disponible y porcentaje utilizado)
-        */
-        long used = RAMUtilities.getMemoryUsed(memory);
-        long total = RAMUtilities.getMemoryTotal(memory);
-        long available = RAMUtilities.getMemoryAvailable(memory);
-        double porcentAvailable = RAMUtilities.getPorcentAvailableMem(memory);
-        double porcentUsed = RAMUtilities.getPorcentUsedMem(memory);
-        
-        return new RAM(total, used, available, porcentAvailable, porcentUsed);
-    } 
+    
 
+    /**
+     * Actualiza los sensores de la GPU y CPU
+     */
+    private void updateSensors(){
+        log.info("Actualizando sensores...");
+        this.components = JSensors.get.components();
+        log.info("Sensores actualizados");
+    }
+
+    private void loadLogger() {
+        // Options: ERROR > WARN > INFO > DEBUG > TRACE
+        System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "INFO");
+        System.setProperty(org.slf4j.impl.SimpleLogger.LOG_FILE_KEY, "System.err");
+        this.log = LoggerFactory.getLogger(PC.class);
+    }
     
 }
